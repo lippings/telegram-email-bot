33ac005e1eddf9b12ce87d62ad40d4d87de23b29
lippings <samuel.lipping@tuni.fi>
2022-04-24 09:16:02 +0300

    Message parsing, rename wrapper, readonly fetching

--- email_utils/email_connection.py
+++ email_utils/email_connection.py
@@ -1,8 +1,14 @@
 import imaplib
 import re
 import logging
+from typing import List, Optional
 
 from file_io import read_yaml
+
+MSG_FIELDS = ['From', 'Message-ID', 'Subject', 'To', 'Date', 'Content-Type']
+MSG_REGEX = {
+    f: re.compile(f'{f}\:\s(?P<value>[^\n\r]+)\r?\n') for f in MSG_FIELDS
+}
 
 
 def get_connection_by_file(path: str):
@@ -14,7 +20,7 @@ def get_connection_by_file(path: str):
     return EmailConnection(email, pwd)
 
 
-def refresh_if_needed(func):
+def _refresh_if_needed(func):
     def wrapper(self, *args, **kwargs):
         try:
             ret = func(self, *args, **kwargs)
@@ -27,6 +33,28 @@ def refresh_if_needed(func):
         return ret
 
     return wrapper
+
+
+class Message:
+    def __init__(self, msg_text: str, msg_flags: str):
+        self.flags = msg_flags
+        self.data = {}
+
+        for field, regex in MSG_REGEX.items():
+            m = regex.search(msg_text)
+
+            if m is None:
+                logging.debug(f'Could not find value {field} in message. Using None')
+                val = None
+            else:
+                val = m.group('value')
+
+            self.data[field] = val
+
+        self.id = self.data['Message-ID']
+
+    def get(self, field: str, default=None):
+        return self.data.get(field, default)
 
 
 class EmailConnection:
@@ -72,10 +100,13 @@ class EmailConnection:
         self.con.logout()
         self.con = None
 
-    def _fetch_in_range(self, min_id: int, max_id: int):
+    def _fetch_in_range(self, min_id: int, max_id: int) -> List[Message]:
         result = []
         for i in range(max_id, min_id, -1):
-            res, msg = self.con.fetch(str(i), '(RFC822)')
+            res, msg_data = self.con.fetch(str(i), '(RFC822)')
+            (_, msg_text), msg_flags = msg_data
+            msg = Message(msg_text.decode(), msg_flags.decode())
+
             result.append(msg)
 
         return result
@@ -84,16 +115,16 @@ class EmailConnection:
         self._disconnect()
         self._connect()
 
-    @refresh_if_needed
-    def fetch_last_k(self, nb_to_fetch: int, folder: str = 'Inbox'):
-        status, msgs = self.con.select(folder)
+    @_refresh_if_needed
+    def fetch_last_k(self, nb_to_fetch: int, folder: str = 'Inbox') -> List[Message]:
+        status, msgs = self.con.select(folder, readonly=True)
         nb_msgs = int(msgs[0])
 
         return self._fetch_in_range(nb_msgs-nb_to_fetch, nb_msgs)
 
-    @refresh_if_needed
-    def fetch_since(self, last_fetched: int, folder: str = 'Inbox'):
-        status, msgs = self.con.select(folder)
+    @_refresh_if_needed
+    def fetch_since(self, last_fetched: int, folder: str = 'Inbox') -> List[Message]:
+        status, msgs = self.con.select(folder, readonly=True)
         nb_msgs = int(msgs[0])
 
         return self._fetch_in_range(last_fetched, nb_msgs)
